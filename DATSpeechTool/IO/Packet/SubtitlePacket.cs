44685c164444a956d48086b897b63c59a6771412
abuali129 <abuali129@gmail.com>
2025-03-21 22:04:25 +0300

    Update SubtitlePacket.cs

--- DATSpeechTool/IO/Packet/SubtitlePacket.cs
+++ DATSpeechTool/IO/Packet/SubtitlePacket.cs
@@ -1,34 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
-
+using System.Linq; // Added for Sum()
 using MGSShared;
 
 namespace DATSpeechTool.IO.Packet
 {
-    /// <summary>
-    ///     Represents a Subtitle Packet.
-    /// </summary>
     public class SubtitlePacketText
     {
         [XmlAttribute]
-        public uint StartTime;
+        public uint StartTime { get; set; }
 
         [XmlAttribute]
-        public uint EndTime;
+        public uint EndTime { get; set; }
 
         [XmlAttribute]
-        public int LanguageId;
+        public int LanguageId { get; set; }
+
+        [XmlAttribute] // New: Text size in bytes (including null terminator)
+        public int TextSize { get; set; }
 
         [XmlText]
-        public string Text;
+        public string Text { get; set; }
 
-        /// <summary>
-        ///     Reads a text entry of a Subtitle Packet from a Stream.
-        /// </summary>
-        /// <param name="Reader">The reader of the Stream where the data is located</param>
-        /// <returns>The entry as a object</returns>
         public static SubtitlePacketText FromStream(EndianBinaryReader Reader)
         {
             SubtitlePacketText PacketText = new SubtitlePacketText();
@@ -41,8 +36,12 @@ namespace DATSpeechTool.IO.Packet
 
             byte[] TextBuffer = new byte[TextLength - 0x10];
             Reader.Read(TextBuffer, 0, TextBuffer.Length);
-            PacketText.Text = MGSText.Buffer2Text(Unpad(TextBuffer), MGSGame.MGS4);
+            TextBuffer = Unpad(TextBuffer); // Trim trailing nulls
+            PacketText.Text = MGSText.Buffer2Text(TextBuffer, MGSGame.MGS4);
             PacketText.Text = PacketText.Text.Replace(Environment.NewLine, "\\n");
+
+            // Calculate TextSize: Length of UNPADDED text (excluding null terminator)
+            PacketText.TextSize = TextBuffer.Length; // Use unpadded length
 
             return PacketText;
         }
@@ -50,7 +49,7 @@ namespace DATSpeechTool.IO.Packet
         private static byte[] Unpad(byte[] Data)
         {
             int Length = 0;
-            while (Length < Data.Length && Data[Length++] != 0);
+            while (Length < Data.Length && Data[Length++] != 0) ;
             return ResizeBuffer(Data, Length - 1);
         }
 
@@ -61,13 +60,9 @@ namespace DATSpeechTool.IO.Packet
             return NewData;
         }
 
-        /// <summary>
-        ///     Writes a text entry of a Subtitle Packet to a Stream.
-        /// </summary>
-        /// <param name="Writer">The writer of the output Stream</param>
-        /// <param name="PacketText">The text entry to be written</param>
         public static void ToStream(EndianBinaryWriter Writer, SubtitlePacketText PacketText)
         {
+            // Unchanged: Existing logic for writing to SPC
             byte[] TextBuffer = new byte[0];
 
             if (PacketText.Text != null)
@@ -92,41 +87,22 @@ namespace DATSpeechTool.IO.Packet
         }
     }
 
-    /// <summary>
-    ///     Represents a packet containing audio subtitles.
-    /// </summary>
     public class SubtitlePacket
     {
         [XmlAttribute]
-        public uint BaseStartTime;
+        public uint BaseStartTime { get; set; }
+
+        [XmlAttribute] // New: Total text size for all Text entries in this subtitle
+        public int TotalTextSize { get; set; }
 
         [XmlArrayItem("Text")]
-        public List<SubtitlePacketText> Texts;
+        public List<SubtitlePacketText> Texts { get; set; } = new List<SubtitlePacketText>();
 
         public SubtitlePacket()
         {
             Texts = new List<SubtitlePacketText>();
         }
 
-        /// <summary>
-        ///     Reads a Subtitle Packet from a Byte Array.
-        /// </summary>
-        /// <param name="Data">The Byte Array where the data is located</param>
-        /// <returns>The packet as a object</returns>
-        public static SubtitlePacket FromBytes(byte[] Data)
-        {
-            using (MemoryStream Input = new MemoryStream(Data))
-            {
-                EndianBinaryReader Reader = new EndianBinaryReader(Input, Endian.Big);
-                return FromStream(Reader);
-            }
-        }
-
-        /// <summary>
-        ///     Reads a Subtitle Packet from a Stream.
-        /// </summary>
-        /// <param name="Reader">The reader of the Stream where the data is located</param>
-        /// <returns>The packet as a object</returns>
         public static SubtitlePacket FromStream(EndianBinaryReader Reader)
         {
             SubtitlePacket Packet = new SubtitlePacket();
@@ -146,41 +122,27 @@ namespace DATSpeechTool.IO.Packet
 
             while (Reader.BaseStream.Position + 0x10 < EndPosition)
             {
-                Packet.Texts.Add(SubtitlePacketText.FromStream(Reader));
+                var TextEntry = SubtitlePacketText.FromStream(Reader);
+                if (TextEntry != null) Packet.Texts.Add(TextEntry);
             }
+
+            // Calculate TotalTextSize after parsing all Text entries
+            Packet.TotalTextSize = Packet.Texts.Sum(t => t.TextSize); // New line
 
             Reader.Seek(EndPosition, SeekOrigin.Begin);
 
             return Packet;
         }
 
-        /// <summary>
-        ///     Writes a Subtitle Packet to a Byte Array.
-        /// </summary>
-        /// <param name="Packet">The packet to be written</param>
-        /// <returns>The array with the data</returns>
-        public static byte[] ToBytes(SubtitlePacket Packet)
-        {
-            using (MemoryStream Output = new MemoryStream())
-            {
-                EndianBinaryWriter Writer = new EndianBinaryWriter(Output, Endian.Big);
-                ToStream(Writer, Packet);
-                return Output.ToArray();
-            }
-        }
-
-        /// <summary>
-        ///     Writes a Subtitle Packet to a Stream.
-        /// </summary>
-        /// <param name="Writer">The writer of the output Stream</param>
-        /// <param name="Packet">The packet to be written</param>
         public static void ToStream(EndianBinaryWriter Writer, SubtitlePacket Packet)
         {
+            // Unchanged: Existing logic for writing to SPC
             using (MemoryStream Content = new MemoryStream())
             {
                 EndianBinaryWriter CWriter = new EndianBinaryWriter(Content, Writer.Endian);
 
-                foreach (SubtitlePacketText Text in Packet.Texts) SubtitlePacketText.ToStream(CWriter, Text);
+                foreach (SubtitlePacketText Text in Packet.Texts) 
+                    SubtitlePacketText.ToStream(CWriter, Text);
 
                 int Length = (int)Content.Length + 0x14 + 1;
                 if ((Length & 0xf) != 0) Length = (Length & ~0xf) + 0x10;
@@ -200,5 +162,15 @@ namespace DATSpeechTool.IO.Packet
                 while ((Writer.BaseStream.Position & 0xf) != 0) Writer.Write((byte)0);
             }
         }
+		
+		public static byte[] ToBytes(SubtitlePacket Packet)
+		{
+			using (MemoryStream Output = new MemoryStream())
+			{
+				EndianBinaryWriter Writer = new EndianBinaryWriter(Output, Endian.Big);
+				ToStream(Writer, Packet);
+				return Output.ToArray();
+			}
+		}
     }
 }
